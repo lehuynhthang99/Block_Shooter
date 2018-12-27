6bd08a7d3e97cc8a41c6724ae8a833392471d87d
btminh1007 <43298552+btminh1007@users.noreply.github.com>
2018-12-27 15:08:15 +0700

    demo B

--- Game1/Program.cs
+++ Game1/Program.cs
@@ -16,9 +16,9 @@ namespace MenuStart
         {
             using (var game = new Game1())
                 game.Run();
-            //Demo1
+            //Demo
         }
     }
 #endif
-    //demo2
+    //demo
 }
